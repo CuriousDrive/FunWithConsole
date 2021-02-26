5c04ffaed89c6b287a6e7c538486523084fc79b0
Fahad Mullaji <fahadmullaji@gmail.com>
2021-02-26 11:52:48 -0500

    1st post created

--- FunWithConsole/Program.cs
+++ FunWithConsole/Program.cs
@@ -1,1 +1,10 @@
-﻿System.Console.Write("Hello");
+﻿using System;
+using System.Linq;
+System.Linq.Enumerable.Range(1, 10).ToList().ForEach(x => 
+ {
+     System.Linq.Enumerable.Range(1, x).ToList().ForEach(y =>
+     {
+         Console.Write(y);
+     });
+     Console.WriteLine();
+ });
