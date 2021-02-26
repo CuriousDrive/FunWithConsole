03f675de07d256fdc724374d753ff5af4ea3262f
Fahad Mullaji <fahadmullaji@gmail.com>
2021-02-26 11:22:49 -0500

    Innial Commit

---
+++ FunWithConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunWithConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
